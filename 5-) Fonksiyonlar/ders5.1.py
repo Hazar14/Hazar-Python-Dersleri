134def4ec5ee8a3a0bfcc1fe799192315a1ef0e2
Recep Hazar Kaya <recephazarkaya@outlook.com>
2026-03-23 20:35:17 +0300

    fonksiyonların neden ve nasıl kullanıldığı öğrenildi.

--- 5-) Fonksiyonlar/ders5.1.py
+++ 5-) Fonksiyonlar/ders5.1.py
@@ -1,28 +1,28 @@
 # method 
 
- # list = [1,2,3,4]
+list = [1,2,3,4]
 
- # list.append (5)
+list.append (5)
 
- # print(type(list))
+print(type(list))
  
- # myString = "hello"
+myString = "hello"
 
- # print(myString.upper())
+print(myString.upper())
 
-
-      #print(list)
-
+print(list)
 
 
-# fonksiyon 
 
-# def sayHello(name= "user"): 
-#     print("hello "+ name)
+#  fonksiyon 
 
-# sayHello("hazar")
-# sayHello("asya") 
-# sayHello()   
+def sayHello(name= "user"): 
+     print("hello "+ name)
+
+sayHello("hazar")
+sayHello("asya") 
+sayHello()   
+
 
 def sayHello(name= "user"): 
     return "hello "+ name
@@ -33,12 +33,12 @@ msg= sayHello("türkan")
 print(msg)
            # bu tarz sıralı durumlarda dosyaya en son atanan değer olduğu için çıktıda sadece türkan görünür.
 
-# def total(x,y) : 
-#     return x + y 
+def total(x,y) : 
+     return x + y 
 
-# result = total (14,37)
+result = total (14,37)
 
-# print(result)
+print(result)
 
 
 def yashesapla(dogumyili) : 
@@ -50,4 +50,20 @@ agecengiz = yashesapla(1970)
 ageelvan = yashesapla(1973)
 print(agehazar,ageasya,ageelvan,agecengiz)
 
- 
+def emeklilik(dogumyili, isim) : 
+    """
+    DOCSTRING : dogum yiliniza gore emeklilige kac yil kaldi.
+    INPUT :  dogum yili.
+    OUTPUT : hesaplanan yil bilgisi.
+    
+    """
+    yas = yashesapla(dogumyili)
+    emeklilik = 65 - yas
+
+    if emeklilik > 0 :
+        print(f"emeklilige {emeklilik} yil kaldi.")
+    else : 
+        print("zaten emeklisiniz.")    
+
+emeklilik(1983,"ali")
+print(help(emeklilik))
